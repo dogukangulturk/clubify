afac2335615d2895c4f099c95a42b5590c884fc5
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2022-10-18 01:39:07 +0300

    Kullanici Etkilesimleri
    React ile kullanici etkilesimleri duzenlendi.

--- frontend/src/UserSignupPage.js
+++ frontend/src/UserSignupPage.js
@@ -2,25 +2,64 @@ import React  from "react";
 
 class UserSignupPage extends React.Component{
 
+    state = {
+        userName : null,
+        displayName : null,
+        password : null,
+        passwordRepeat : null
+    };
+
+    onChange = event => {
+        const field = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [field]: value
+        })
+    }
+
+    // onChangeUserName = event => {
+    //     this.setState({
+    //         userName: event.target.value
+    //     })
+    // };
+
+    // onChangeDisplayName = event => {
+    //     this.setState({
+    //         displayName: event.target.value
+    //     })
+    // };
+
+    // onChangePassword = event => {
+    //     this.setState({
+    //         displayName: event.target.value
+    //     })
+    // };
+
+    // onChangePasswordRepeat = event => {
+    //     this.setState({
+    //         displayName: event.target.value
+    //     })
+    // };
+
     render(){
         return(
             <form>
                 <h1>Sign Up</h1>
                 <div>
                     <label>Username</label>
-                    <input />
+                    <input name="userName" onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Display Name</label>
-                    <input />
+                    <input name="displayName" onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" />
+                    <input name="password" type="password" onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Password Repeat</label>
-                    <input type="password" />
+                    <input name="passwordRepeat" type="password" onChange={this.onChange} />
                 </div>
                 <button>Sign Up</button>
             </form>

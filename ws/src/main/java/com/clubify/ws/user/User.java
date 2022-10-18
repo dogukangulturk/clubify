c05c32711f3efaaea676f55b04609ecb0439fcb0
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2022-10-18 13:58:55 +0300

    h2-database
    database baglantisi olusturuldu gerekli duzenlemeler yapildi

--- ws/src/main/java/com/clubify/ws/user/User.java
+++ ws/src/main/java/com/clubify/ws/user/User.java
@@ -1,9 +1,20 @@
 package com.clubify.ws.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name="users")
 public class User {
+	
+	@Id
+	@GeneratedValue
+	private long id;
 	
 	private String username;
 	

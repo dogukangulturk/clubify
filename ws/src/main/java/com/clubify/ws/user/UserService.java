61281e05eb40a55fc7daf4c240d475b91165fcb7
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2022-10-18 21:10:15 +0300

    sifreleme islemleri
    Spring Security kullanarak sifreleme islemleri yapildi

--- ws/src/main/java/com/clubify/ws/user/UserService.java
+++ ws/src/main/java/com/clubify/ws/user/UserService.java
@@ -1,6 +1,7 @@
 package com.clubify.ws.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,15 +9,17 @@ public class UserService {
 	
 	UserRepository userRepository;
 	
+	PasswordEncoder passwordEncoder;
 	
-	//@Autowired
 	public UserService(UserRepository userRepository) {
 		this.userRepository = userRepository;
+		this.passwordEncoder = new BCryptPasswordEncoder();
 	}
 
 
 
 	public void save(User user) {
+		user.setPassword(this.passwordEncoder.encode(user.getPassword()));
 		userRepository.save(user);
 	}
 

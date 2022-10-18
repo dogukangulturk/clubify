50ce7c968f31a26eb9aae0411d6c77078ae9ad8e
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2022-10-18 20:29:03 +0300

    refactoring

--- ws/src/main/java/com/clubify/ws/user/UserController.java
+++ ws/src/main/java/com/clubify/ws/user/UserController.java
@@ -1,21 +1,24 @@
 package com.clubify.ws.user;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.clubify.ws.shared.GenericResponse;
 
 @RestController
 public class UserController {
 	
 	@Autowired
-	UserRepository userRepository;
+	UserService userService;
 
 	@PostMapping("/api/1.0/users")
-	public void createUser(@RequestBody User user) {
-		userRepository.save(user);
+	public GenericResponse createUser(@RequestBody User user) {
+		userService.save(user);
+		return new GenericResponse("User Created");
 	}
 	
 }

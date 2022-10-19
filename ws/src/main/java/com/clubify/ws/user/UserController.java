5cab2ae8ae423fb989692a6a03cba46e081ecbec
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2022-10-19 11:31:51 +0300

    username validation

--- ws/src/main/java/com/clubify/ws/user/UserController.java
+++ ws/src/main/java/com/clubify/ws/user/UserController.java
@@ -1,12 +1,17 @@
 package com.clubify.ws.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.clubify.ws.error.ApiError;
 import com.clubify.ws.shared.GenericResponse;
 
 @RestController
@@ -16,9 +21,17 @@ public class UserController {
 	UserService userService;
 
 	@PostMapping("/api/1.0/users")
-	public GenericResponse createUser(@RequestBody User user) {
+	public ResponseEntity<?> createUser(@RequestBody User user) {
+		String username = user.getUsername();
+		if (username == null || username.isEmpty()) {
+			ApiError error = new ApiError(400, "Validation Error", "/api/1.0/users");
+			Map<String, String> validatationErrors = new HashMap<>();
+			validatationErrors.put("username", "Username cannot be null");
+			error.setValidationErrors(validatationErrors);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		}
 		userService.save(user);
-		return new GenericResponse("User Created");
+		return ResponseEntity.ok(new GenericResponse("user created"));
 	}
 	
 }

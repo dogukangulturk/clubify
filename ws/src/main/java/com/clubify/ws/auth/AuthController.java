59788d9cd833f3c7d9b6c3e3535647cb5fe7b3b9
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2022-11-30 18:38:09 +0300

    Authentication-Hata Mesajlari

--- ws/src/main/java/com/clubify/ws/auth/AuthController.java
+++ ws/src/main/java/com/clubify/ws/auth/AuthController.java
@@ -1,17 +1,12 @@
 package com.clubify.ws.auth;
 
-import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.clubify.ws.error.ApiError;
+import com.clubify.ws.shared.CurrentUser;
 import com.clubify.ws.shared.Views;
 import com.clubify.ws.user.User;
 import com.clubify.ws.user.UserRepository;
@@ -24,33 +19,11 @@ public class AuthController {
 	@Autowired
 	UserRepository userRepository;
 	
-	PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 	
 	@PostMapping("/api/1.0/auth")
 	@JsonView(Views.Base.class)
-	ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization", required = false) String authorization) {
-		if (authorization == null) {
-			ApiError error = new ApiError(401, "Unauthorization request", "/api/1.0/auth");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-		}
-		String base64encoded = authorization.split("Basic ")[1];
-		String decoded = new String(Base64.getDecoder().decode(base64encoded));
-		String[] parts = decoded.split(":");
-		String username = parts[0];
-		String password = parts[1];
-		User inDB = userRepository.findByUsername(username);
-		
-		if (inDB == null) {
-			ApiError error = new ApiError(401, "Unauthorization request", "/api/1.0/auth");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-		}
-		String hashedPassword = inDB.getPassword();
-		if (!passwordEncoder.matches(password, hashedPassword)) {
-			ApiError error = new ApiError(401, "Unauthorization request", "/api/1.0/auth");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-		}
-		
- 		return ResponseEntity.ok(inDB);
+	ResponseEntity<?> handleAuthentication(@CurrentUser User user) {
+ 		return ResponseEntity.ok(user);
 	}
 
 }
--- ws/src/main/java/com/clubify/ws/user/UserController.java
+++ ws/src/main/java/com/clubify/ws/user/UserController.java
@@ -1,21 +1,13 @@
 package com.clubify.ws.user;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.clubify.ws.error.ApiError;
 import com.clubify.ws.shared.GenericResponse;
 
 @RestController
@@ -30,18 +22,6 @@ public class UserController {
 		userService.save(user);
 		return new GenericResponse("user created");
 		
-	}
-	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ApiError handleValidationExpection(MethodArgumentNotValidException exception) {
-		ApiError error = new ApiError(400, "Validation Error", "/api/1.0/users");
-		Map<String, String> validatationErrors = new HashMap<>();
-		for (FieldError fieldError: exception.getBindingResult().getFieldErrors()) {
-			validatationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-		}
-		error.setValidationErrors(validatationErrors);
-		return error;
 	}
 	
 }
